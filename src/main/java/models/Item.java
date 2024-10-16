721aca2bebd896bc63bc1e3fa6c00c464c534872
247720 Piotr Lewkowicz <247720@edu.p.lodz.pl>
2024-10-16 21:57:57 +0200

    Changed inheritance strategy

--- src/main/java/models/Item.java
+++ src/main/java/models/Item.java
@@ -3,7 +3,7 @@ package models;
 import jakarta.persistence.*;
 
 @Entity
-@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
+@Inheritance(strategy = InheritanceType.TABLE_PER_CLASS)
 @Table(name = "items")
 public class Item {
 

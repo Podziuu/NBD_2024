75310e1ec22bbc2aa28d5ecf120dcc752b1425d0
Bartosz Podemski <bartekpodemski@gmail.com>
2024-11-12 14:32:23 +0100

    Docker fix and clientmanager

--- src/main/java/managers/ClientManager.java
+++ src/main/java/managers/ClientManager.java
@@ -1,37 +1,60 @@
-//package managers;
-//
-//import models.Client;
-//import models.ClientType;
-//
-//import java.util.ArrayList;
-//import java.util.List;
-//import java.util.Optional;
-//
-//public class ClientManager {
-//    private List<Client> clients;
-//
-//    public ClientManager() {
-//        this.clients = new ArrayList<>();
-//    }
-//
-//    public String report() {
-//        StringBuilder report = new StringBuilder();
-//        for (Client client : clients) {
-//            report.append(client.getInfo()).append("\n");
-//        }
-//        return report.toString();
-//    }
-//
-//    public List<Client> getAll() {
-//        return clients;
-//    }
-//
-//    public Optional<Client> find(long personalId) {
-//        return clients.stream().filter(client -> client.getPersonalId().equals(personalId)).findFirst();
-//    }
-//
-//    public void registerClient(String firstName, String lastName, String personalId, ClientType clientType) {
-//        Client newClient = new Client(personalId, firstName, lastName, clientType);
-//        clients.add(newClient);
-//    }
-//}
+package managers;
+
+import org.bson.types.ObjectId;
+import repos.ClientRepository;
+
+import models.Client;
+import models.ClientType;
+import models.DiamondMembership;
+import models.Membership;
+import models.NoMembership;
+
+public class ClientManager {
+    private final ClientRepository clientRepository;
+
+    public ClientManager(ClientRepository clientRepository) {
+        this.clientRepository = clientRepository;
+    }
+
+    public void addClient(String firstName, String lastName, long personalID, String clientType) {
+        ClientType type = getClientType(clientType);
+        clientRepository.addClient(new Client(personalID, firstName, lastName, type));
+    }
+
+    public Client getClient(ObjectId id) {
+        return clientRepository.getClient(id);
+    }
+
+    public void updateClient(ObjectId id, String firstName, String lastName, String clientType) {
+        Client client = clientRepository.getClient(id);
+        client.setFirstName(firstName);
+        client.setLastName(lastName);
+        client.setClientType(getClientType(clientType));
+        clientRepository.updateClient(client);
+    }
+
+    public void removeClient(ObjectId id) {
+        clientRepository.removeClient(id);
+    }
+
+    public void archiveClient(ObjectId id) {
+        Client client = clientRepository.getClient(id);
+        client.setArchive(true);
+        clientRepository.updateClient(client);
+    }
+
+    public void unarchiveClient(ObjectId id) {
+        Client client = clientRepository.getClient(id);
+        client.setArchive(false);
+        clientRepository.updateClient(client);
+    }
+
+    private ClientType getClientType(String clientType) {
+        return switch (clientType) {
+            case "DiamondMembership" -> new DiamondMembership();
+            case "Membership" -> new Membership();
+            case "NoMembership" -> new NoMembership();
+            default -> throw new IllegalArgumentException("Invalid client type");
+        };
+    }
+}
